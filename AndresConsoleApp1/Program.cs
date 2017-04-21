b205337bc26769c8d564ea8d158bc014c577e6cf
Andres Velasquez <andres.velasquez@me.com>
2017-04-21 10:33:53 -0700

    minor updates

--- AndresConsoleApp1/Program.cs
+++ AndresConsoleApp1/Program.cs
@@ -33,21 +33,21 @@ namespace AndresConsoleApp1
 			while ((line = file.ReadLine()) != null) // Assigns, one by one, each line in the read file to variable "line". Next block executes if line is not null.
 			{
 				Console.WriteLine(line);
-				bool checkIfInteger = Int32.TryParse(line, out int number);
-				if (!(checkIfInteger) || number < 1) // I'm only going to consider positive integers
+				bool checkIfInteger = int.TryParse(line, out int number); // Seems int is shorthand for Int32 so I'll just use that as the limit.
+				if (!(checkIfInteger) || number < 1) // I'm only going to consider positive integers. Probably can't have commas in them because I don't think that will pass the TryParse line above.
 				{
 					Console.WriteLine(line + " is not an integer greater than 1" + Environment.NewLine);
 					counter++;
 					continue;
 				}
-				if (Convert.ToInt64(line) == 1)
+				if (Convert.ToInt32(line) == 1) // Seems like they should have a "ToInt" as shorthand since int exists.
 				{
 					Console.WriteLine("The loneliest number? Yes. Prime? No." + Environment.NewLine); // I could just make the above check "< 2" instead of 1 but then I wouldn't get to make this corny joke!
 					counter++;
 					continue;
 				}
-				Factorizer doThat = new Factorizer(); // Seems I need an instance of this class to call its method. OOP gone crazy?
-				List<long> factors = doThat.Factorize(Convert.ToInt64(line)); // Lines from the file appear to be put into strings so have to cast to long before calling Factorize.
+				Factorizer factorizeWorker = new Factorizer(); // Seems I need an instance of this class to call its method. OOP gone crazy?
+				List<int> factors = factorizeWorker.Factorize(Convert.ToInt32(line)); // Lines from the file appear to be put into strings so have to cast to long before calling Factorize.
 				if (factors.Count == 1)
 				{
 					Console.WriteLine(line + " is prime." + Environment.NewLine); // If factorize returned a list containing just one thing (which will be the original number passed), that means the number is prime so I'll just print that.
@@ -61,9 +61,9 @@ namespace AndresConsoleApp1
 			file.Close(); // I think I have to close this for garbage collection purposes? I also saw that using the keyword "using" with file may dispose of it automatically. Memory management and garbage collection were not things I dealt with in the higher level languages I studied!
 		}
 
-		private List<long> Factorize(long num) // This nice algorithm came from Rod Stephens. Had to change it a bit to make it work. He allows its use and requests a shout out in the comments if used. Hi Rod! I enjoy algorithms and was pretty good at them but haven't looked at them in months, busy reviewing my old IT knowledge.
+		private List<int> Factorize(int num) // This nice algorithm came from Rod Stephens. Had to change it a bit to make it work. He allows its use and requests a shout out in the comments if used. Hi Rod! I enjoy algorithms and was pretty good at them but haven't looked at them in months, busy reviewing my old IT knowledge.
 		{
-			List<long> result = new List<long>();
+			List<int> result = new List<int>();
 			while (num % 2 == 0) // Get all the 2's possible first
 			{
 				result.Add(2);
@@ -71,7 +71,7 @@ namespace AndresConsoleApp1
 			}
 			// I think that starting with 3 and going up works because any higher numbers that aren't prime will have been taken care of by an earlier iteration which is prime. Definitely need to practice these more.
 			// Take out other primes.
-			long factor = 3;
+			int factor = 3;
 			while (factor * factor <= num) // Go until the square of variable "factor" becomes larger than the number being factored
 			{
 				if (num % factor == 0)
